0bacfbab483a5b78f2e86f9454a0e13edb0ceff1
SKrishna-7 <sureshkrishna375@gmail.com>
2025-02-09 18:12:50 +0530

    EDA and Training

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+import logger
 
 def error_msg_detail(err,err_detail:sys):
     _,_,exc_tb=err_detail.exc_info()
@@ -20,9 +20,9 @@ class CustomException(Exception):
         
 
 
-# if __name__ == '__main__':
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Divide by Zero..")
-#         raise  CustomException(e,sys)
+if __name__ == '__main__':
+    try:
+        a=1/0
+    except Exception as e:
+        logger.logging.info("Divide by Zero..")
+        raise  CustomException(e,sys)
